48d58e015051053a378013acc58ab2655f3ba9cb
masdika ID <masdikaid@gmail.com>
2021-04-05 14:35:52 +0700

    create user form view

---
+++ app/Views/content/users/create_user.php
@@ -0,0 +1,25 @@
+<?= $this->extend('layout/default') ?>
+<?= $this->section('content') ?>
+<h2>Create Users</h2>
+
+
+<?= form_open('users/create') ?>
+    <label for="name">name</label>
+    <input type="text" name="name" id="name">
+    </br>
+    <label for="name">email</label>
+    <input type="email" name="email" id="email">
+    </br>
+    <label for="name">password</label>
+    <input type="password" name="password" id="password">
+    </br>
+    <label for="number">phone</label>
+    <input type="text" name="phone" id="phone">
+    </br>
+    <input type="submit" value="submit">
+<?= form_close() ?>
+
+<?= \Config\Services::validation()->listErrors() ?>
+
+<?= $this->endSection() ?>
+

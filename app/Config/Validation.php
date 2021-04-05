e8e9eae136a9d8129e689f9d405c1ad4ddbf3c68
masdika ID <masdikaid@gmail.com>
2021-04-05 14:24:11 +0700

    add user validation rule

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -37,6 +37,41 @@ class Validation
 		'single' => 'CodeIgniter\Validation\Views\single',
 	];
 
+	public $users = [
+		'name' => [
+			'rules' => 'required|min_length[3]',
+			'errors' => [
+				'required' => 'you must fill your name.',
+				'min_length' => 'the minimum character is 3 or more.'
+			]
+		],
+		'password' => [
+			'rules' => 'required|min_length[8]',
+			'errors' => [
+				'required' => 'you must fill your password.',
+				'min_length' => 'the minimum character is 8 or more.'
+			]
+		],
+		'email' => [
+			'rules' => 'required|valid_email|is_unique[users.email]',
+			'errors' => [
+				'required' => 'you must fill your email.',
+				'valid_email' => 'you must fill the valid email.',
+				'is_unique' => 'email already exists'
+			]
+		],
+		'phone' => [
+			'rules' => 'required|numeric|min_length[9]|max_length[13]|is_unique[users.phone]',
+			'errors' => [
+				'required' => 'you must fill your phone number.',
+				'min_length' => 'your phone number not valid.',
+				'max_length' => 'your phone number not valid.',
+				'is_unique' => 'phone number already exists'
+			]
+		]
+
+	];
+
 	//--------------------------------------------------------------------
 	// Rules
 	//--------------------------------------------------------------------

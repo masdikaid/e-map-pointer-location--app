9f78b0aec2eef96813c9251efb66f9fe509a59e6
masdika ID <masdikaid@gmail.com>
2021-04-05 14:43:30 +0700

    user model tester

---
+++ tests/app/Model/UserModelTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Support;
+
+use Tests\Support\Database\DatabaseTest;
+use CodeIgniter\Test\CIDatabaseTestCase;
+
+class UserDatabaseTest extends CIDatabaseTestCase
+{
+	protected $migrate = true;
+    protected $refresh  = true;
+    protected $seed = "UserSeeder";
+
+
+	public function setUp(): void
+	{
+		parent::setUp();
+	}
+
+	public function tearDown(): void
+	{
+		parent::tearDown();
+	}
+
+	public function userFindAllTest()
+	{
+		$model = model('UserModel');
+		$objects = $model->findAll();
+
+		$this->assertCount(30, $objects);
+	}
+
+	public function UserSoftDeleteTests()
+	{
+        $model = model('UserModel');
+
+		$object = $model->first();
+		$model->delete($object->id);
+
+		$this->assertNull($model->find($object->id));
+
+		$criteria = ['id' => $object->id, 'deleted_at' => null];
+        $this->dontSeeInDatabase('users', $criteria);
+
+        $criteria = ['deleted_at' => null];
+        $this->seeNumRecords(29, 'users', $criteria);
+	}
+}

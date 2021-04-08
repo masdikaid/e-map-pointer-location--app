76815bf46dde1725c55c01d119fd26f3a08906a9
masdika ID <masdikaid@gmail.com>
2021-04-08 16:24:28 +0700

    add user list, single list and delete user

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -8,14 +8,33 @@ use App\Entities\UserEntity;
 class Users extends BaseController
 {
 
-    public function index()
+    public function index(int $id = null)
     {
-        $model = model('UserModel');
-        $res = $model->findAll();
-        $this->view->setData([
-            'users' => $res
-        ]);
-        return $this->view->render('content/users/user_list');
+        if (!$id)
+        {
+            $model = model('UserModel');
+            $res = $model->findAll();
+            $this->view->setData([
+                'users' => $res
+            ]);
+            return $this->view->render('content/users/user_list');
+        }
+        else 
+        {
+            $model = model('UserModel');
+
+            if ($res = $model->find($id))
+            {
+                $this->view->setData([
+                    'user' => $res
+                    ]);
+                return $this->view->render('content/users/single_user');
+            }
+            else 
+            {
+                throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+            }
+        }
     }
     
     public function create()
@@ -43,6 +62,24 @@ class Users extends BaseController
         else 
         {
             return $this->view->render('content/users/create_user');
+        }
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function delete(int $id)
+    {
+        $model = model('UserModel');
+        if ($res = $model->delete($id))
+        {
+            return redirect()->to('/users');
+        }
+        else 
+        {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
         }
     }
 }

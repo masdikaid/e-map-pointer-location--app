618d16aaf8a3edb8b059a291e3d915df9522754d
masdika ID <masdikaid@gmail.com>
2021-04-09 13:30:03 +0700

    add new test : user list test, single user test, test given invalid user, test given string user, test delete etc

--- tests/app/Controller/UserControllerTest.php
+++ tests/app/Controller/UserControllerTest.php
@@ -2,24 +2,70 @@
 
 namespace Tests\App\Controllers;
 use CodeIgniter\Test\FeatureTestCase;
+use Tests\App\Models\UserFabricator;
 
 
 
 class UserControllerTest extends FeatureTestCase
 {
 	protected $migrateOnce = true;
+	protected $seedOnce = true;
     protected $refresh = true;
+	protected $seed = 'UserSeeder';
 	protected $basePath = APPPATH . 'Database';
 	protected $namespace = 'App';
-
+	
 	public function setUp(): void
 	{
 		parent::setUp();
 	}
-
+	
 	public function tearDown(): void
 	{
 		parent::tearDown();
+	}
+	
+	public function testUserList()
+	{
+		$res = $this->call('get', '/users');
+		$res->assertOK();
+		$res->assertSee('User List');
+	}
+	
+	
+	public function testSingleUser()
+	{
+		$user = $this->model->first();
+		$res = $this->call('get', "/users/index/{$user->id}");
+		$res->assertOK();
+		$res->assertSee($user->name);
+	}
+
+	public function testGivenStringInSingleUser()
+	{
+		$this->expectException(\TypeError::class);
+		$res = $this->call('get', "/users/index/as");
+	}
+	
+	public function testInvalidSingleUser()
+	{
+		$this->expectException(\CodeIgniter\Exceptions\PageNotFoundException::class);
+		$res = $this->call('get', "/users/index/100");
+	}
+
+	public function testDeleteUser()
+	{
+		$user = $this->model->first();
+		$res = $this->call('get', "/users/delete/{$user->id}");
+		$res->assertRedirect();
+
+		$this->seeInDatabase('users', ['id' => $user->id]);
+	}
+
+	public function testInvalidDeleteUser()
+	{
+		$this->expectException(\CodeIgniter\Exceptions\PageNotFoundException::class);
+		$res = $this->call('get', "/users/delete/100");
 	}
 
 	public function testGetCreateUser()
